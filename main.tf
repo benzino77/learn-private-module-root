2dfb593951a3d888a1b7dc00619747a9c33ec239
benzino77 <15658235+benzino77@users.noreply.github.com>
2024-03-16 11:50:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,9 @@ provider "aws" {
 
 module "s3-webapp" {
   source  = "app.terraform.io/piotrantczak/s3-webapp/aws"
+  version = "1.0.0"
+  # insert required variables here
   name    = var.name
   region  = var.region
   prefix  = var.prefix
-  version = "1.0.0"
 }
